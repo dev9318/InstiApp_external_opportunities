e130640be0fc5288080040fb6d169f8ecb3aa0b8
dev9318 <dev.moxaj@gmail.com>
2021-10-10 20:38:31 +0530

    Update Blog to show posts from 1 hr

--- blogs/feeds.py
+++ blogs/feeds.py
@@ -3,13 +3,18 @@ from django.urls import reverse_lazy
 
 from .models import Blog, body
 
+from datetime import timedelta
+from django.utils import timezone
+
 class BlogFeed(Feed):
     title = "Blogs added today"
     link = '/blog/feed/'
     description = "RSS feed of the blogs"
 
     def items(self):
-        return Blog.objects.all()
+        current_time = timezone.now().replace(minute=0, second=0, microsecond=0)
+        one_hour_before = current_time - timedelta(hours=1)
+        return Blog.objects.filter(updated__range=(one_hour_before,current_time))
 
     def item_title(self, item):
         return item.title
